89e24630763d7b4114a7ad071e78d338a2995568
David <davidbrandon713@gmail.com>
2022-12-06 20:59:10 -0600

    first commit 2022/12/06

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
+import { legacy_createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
+import { searchRobots } from './reducers';
 import 'tachyons';
 // import registerServiceWorker from './registerServiceWorker';
 
+const store = legacy_createStore(searchRobots);
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>);
 
 
 // ReactDOM.render(<App />, document.getElementById('root'));
