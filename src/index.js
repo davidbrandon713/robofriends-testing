c0b81de2a6b9237f8b63f1837750e452a63ed24b
David <davidbrandon713@gmail.com>
2022-12-09 15:16:38 -0600

    2nd commit 2022/12/09

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
-import { legacy_createStore } from 'redux';
+import { legacy_createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-const store = legacy_createStore(searchRobots);
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 const root = createRoot(document.getElementById('root'));
 root.render(
@@ -19,4 +24,4 @@ root.render(
 
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
